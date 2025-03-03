b0f2a69f4305fd57eeb399c20abf8e58aaa6949a
Bastian Erfurth <bastian.erfurth01@gmail.com>
2025-03-03 11:23:59 +0100

    test 1 laut Tasksheet Aufgabe 1

---
+++ main.dart
@@ -0,0 +1,12 @@
+void main() {
+  int age = 15;
+  bool hasParentalConsent = true;
+  int movieAgeRating = 16;
+
+  if (age >= movieAgeRating) {
+    print("OK für Filmabend");
+  } else {
+    (hasParentalConsent && age >= (movieAgeRating - 2));
+    print("Filmabend erlaubt");
+  }
+}
